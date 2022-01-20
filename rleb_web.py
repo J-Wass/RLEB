49f786b368351611e5ba3342d38eafc24b387047
j-wass <jonahwasserman97@gmail.com>
2022-01-19 21:39:33 -0800

    reduce required flask version

--- rleb_web.py
+++ rleb_web.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    status = ['RLEB Status']
+    status = ['.::RLEB Status::.']
 
     # Asyncio
     asyncio_threads = ['--Discord Lightweight Threads (asyncio)--\n']
