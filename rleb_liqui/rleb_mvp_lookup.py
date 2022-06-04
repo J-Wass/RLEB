93315229bb6b3a91a153636df16a6fa7e6d9d429
j-wass <jonahwasserman97@gmail.com>
2022-06-03 22:57:13 -0700

    black format everything & fix !echo

--- rleb_liqui/rleb_mvp_lookup.py
+++ rleb_liqui/rleb_mvp_lookup.py
@@ -40,23 +40,29 @@ You can view the list of previous MVP polls and results [over here.](https://www
 
 footer = "You can view the list of previous MVP polls and results [over here.](https://www.reddit.com/r/RocketLeagueEsports/search?q=selftext%3A%22MVP%22+%22Results%22+author%3ARLMatchThreads+subreddit%3ARocketLeagueEsports&sort=new&t=all)"
 
+
 class MVPCandidates:
-    """ Encapsulation of a group of candidates for a specific mvp voting round. """
-    def __init__(self,title: str, candidates: list[str]):
+    """Encapsulation of a group of candidates for a specific mvp voting round."""
+
+    def __init__(self, title: str, candidates: list[str]):
         self.title = title
         self.candidates = candidates
 
-async def handle_mvp_form_creation(liquipedia_urls: list[str] , channel) -> None:
+
+async def handle_mvp_form_creation(liquipedia_urls: list[str], channel) -> None:
     """Handles a request to get markdown for a new post about mvps from each liqui url."""
     candidate_groups = await _get_mvp_candidates(liquipedia_urls, channel)
     if candidate_groups is None or len(candidate_groups) == 0:
         await channel.send(f"Could't find mvp candidates from {channel}.")
-    
+
     markdown = await _get_mvp_markdown(candidate_groups, channel)
     if markdown:
-        await rleb_stdout.print_to_channel(channel, markdown, title="RLCS 21-22 | Weekend MVP Poll")
+        await rleb_stdout.print_to_channel(
+            channel, markdown, title="RLCS 21-22 | Weekend MVP Poll"
+        )
     else:
         await channel.send(f"Could't find mvp candidates from {channel}.")
+
 
 async def handle_mvp_results_lookup(form_url: str, channel) -> None:
     """Fetches the results of a google form for the supplied form edit url."""
@@ -67,21 +73,30 @@ async def handle_mvp_results_lookup(form_url: str, channel) -> None:
     except Exception as e:
         await channel.send(str(e))
         await channel.send(f"\nCouldn't get form responses for {form_url}.")
-    
+
     # For each region, create markdown using the form responses.
     region_markdowns = []
     for region, top_5_players in top_5_for_each_region.items():
-        markdown = mvp_results_template.replace('{REGION_NAME}', region)
+        markdown = mvp_results_template.replace("{REGION_NAME}", region)
         top_5_players.sort(key=lambda x: x[1], reverse=True)
         for i, player_tuple in enumerate(top_5_players, start=1):
             name = player_tuple[0].split("(")[0].strip()
             team = player_tuple[0].split("(")[1].split(")")[0].strip()
-            markdown = markdown.replace(f'{{PLAYER{i}}}', name).replace(f'{{PLAYER{i}_PERCENT}}', str(round(player_tuple[1],1))).replace(f'{{PLAYER{i}_TEAM}}', team)
+            markdown = (
+                markdown.replace(f"{{PLAYER{i}}}", name)
+                .replace(f"{{PLAYER{i}_PERCENT}}", str(round(player_tuple[1], 1)))
+                .replace(f"{{PLAYER{i}_TEAM}}", team)
+            )
         region_markdowns.append(markdown)
     markdown = "\n".join(region_markdowns) + "\n" + footer
-    await rleb_stdout.print_to_channel(channel, markdown, "RLCS | Week After MVP Poll | RESULTS")
+    await rleb_stdout.print_to_channel(
+        channel, markdown, "RLCS | Week After MVP Poll | RESULTS"
+    )
 
-async def _get_mvp_candidates(liquipedia_urls: list[str] , channel) -> list[MVPCandidates]:
+
+async def _get_mvp_candidates(
+    liquipedia_urls: list[str], channel
+) -> list[MVPCandidates]:
     """Gets a list of MVPCandidates for each url in liquipedia_urls."""
     candidate_groups = []
     for url in liquipedia_urls:
@@ -90,16 +105,19 @@ async def _get_mvp_candidates(liquipedia_urls: list[str] , channel) -> list[MVPC
             candidate_groups.append(candidates)
     return candidate_groups
 
-async def _get_single_mvp_candidate_group(liquipedia_url: str , channel) -> Optional[MVPCandidates]:
+
+async def _get_single_mvp_candidate_group(
+    liquipedia_url: str, channel
+) -> Optional[MVPCandidates]:
     """Gets an MVPCandidate group from the liquipedia_url."""
     page = None
     try:
-         page = rleb_liqui_utils.get_page_html_from_url(liquipedia_url)
+        page = rleb_liqui_utils.get_page_html_from_url(liquipedia_url)
     except Exception as e:
         await channel.send("Couldn't load {0}!\nError: {1}".format(liquipedia_url, e))
         rleb_settings.rleb_log_info(
-                "MVP: Couldn't load {0}!\nError: {1}".format(liquipedia_url, e)
-            )
+            "MVP: Couldn't load {0}!\nError: {1}".format(liquipedia_url, e)
+        )
         rleb_settings.rleb_log_error(traceback.format_exc())
         return None
 
@@ -119,7 +137,7 @@ async def _get_single_mvp_candidate_group(liquipedia_url: str , channel) -> Opti
         candidates = []
         for i in range(3):
             player_name = players[i].text.strip()
-            candidates.append(f'{player_name} ({team_name})')
+            candidates.append(f"{player_name} ({team_name})")
         mvp_candidates[team_name] = candidates
 
     # Get all players on top teams.
@@ -133,14 +151,21 @@ async def _get_single_mvp_candidate_group(liquipedia_url: str , channel) -> Opti
     title = " | ".join(liquipedia_url.split("/")[4:]).replace("_", " ")
     return MVPCandidates(title, eligible_candidates)
 
+
 async def _get_mvp_markdown(candidate_groups: list[MVPCandidates], channel):
     """Creates a new google form for the candidate groups, and returns reddit markdown for it."""
     form_link = await _create_mvp_form(candidate_groups, channel)
     return mvp_markdown_template.replace("{FORM_LINK}", form_link)
 
-async def _create_mvp_form(candidate_groups: list[MVPCandidates], channel) -> Optional[str]:
+
+async def _create_mvp_form(
+    candidate_groups: list[MVPCandidates], channel
+) -> Optional[str]:
     """Creates a google form, with one page for each group of mvp candidates."""
-    SCOPES = ["https://www.googleapis.com/auth/drive","https://www.googleapis.com/auth/drive.metadata"]
+    SCOPES = [
+        "https://www.googleapis.com/auth/drive",
+        "https://www.googleapis.com/auth/drive.metadata",
+    ]
     credential_info = json.loads(rleb_settings.GOOGLE_CREDENTIALS_JSON)
     credentials = service_account.Credentials.from_service_account_info(
         credential_info, scopes=SCOPES
@@ -152,9 +177,13 @@ async def _create_mvp_form(candidate_groups: list[MVPCandidates], channel) -> Op
         return None
     _add_permissions_to_formid(form_data["formId"], credentials)
 
-    edit_msg = await channel.send(f"Edit the form here: https://docs.google.com/forms/d/{form_data['formId']}/edit", embed=None)
+    edit_msg = await channel.send(
+        f"Edit the form here: https://docs.google.com/forms/d/{form_data['formId']}/edit",
+        embed=None,
+    )
     await edit_msg.edit(suppress=True)
     return form_data["responderUri"]
+
 
 def _get_mvp_form_responses(form_link: str) -> Dict[str, List[Tuple[str, float]]]:
     """
@@ -162,7 +191,10 @@ def _get_mvp_form_responses(form_link: str) -> Dict[str, List[Tuple[str, float]]
     MVP winners are represented as a Tuple[player_name, percent_won].
     """
 
-    SCOPES = ["https://www.googleapis.com/auth/drive","https://www.googleapis.com/auth/drive.metadata"]
+    SCOPES = [
+        "https://www.googleapis.com/auth/drive",
+        "https://www.googleapis.com/auth/drive.metadata",
+    ]
     credential_info = json.loads(rleb_settings.GOOGLE_CREDENTIALS_JSON)
     credentials = service_account.Credentials.from_service_account_info(
         credential_info, scopes=SCOPES
@@ -175,39 +207,41 @@ def _get_mvp_form_responses(form_link: str) -> Dict[str, List[Tuple[str, float]]
     # Mapping of Region -> list of tuple (name, percent)
     # ex) 'APAC N' -> [('Maxeew (Gaimin Gladiators)', 33.3), ('LCT (Gaimin Gladiators)', 26.7), ('Abscrazy (Gaimin Gladiators)', 8.9)]
     return_dict = {}
-    
+
     # Mapping of question_id to name.
     question_names = {}
     result = service.forms().get(formId=form_id).execute()
-    for item in result['items']:
-        question_id = item['questionItem']['question']['questionId']
-        question_name = item['title'].split("|")[-2].strip()
+    for item in result["items"]:
+        question_id = item["questionItem"]["question"]["questionId"]
+        question_name = item["title"].split("|")[-2].strip()
         question_names[question_id] = question_name
-
 
     # Mapping of question_id to list of answers
     question_bucket = {}
     result = service.forms().responses().list(formId=form_id).execute()
-    responses = result['responses']
+    responses = result["responses"]
     for r in responses:
-        for question_id, answers in r['answers'].items():
+        for question_id, answers in r["answers"].items():
             # If question_id hasn't been seen, add a bucket.
             if question_id not in question_bucket:
                 question_bucket[question_id] = []
 
-            answer = answers['textAnswers']['answers'][0]['value']
+            answer = answers["textAnswers"]["answers"][0]["value"]
             question_bucket[question_id].append(answer)
 
     # Get top 5 for each question.
     for question_id, answers in question_bucket.items():
         top_5_frequency = Counter(answers).most_common(5)
         number_of_answers = len(answers)
-        top_5_percent = list(map(lambda x: (x[0],100*x[1]/number_of_answers,1), top_5_frequency))
+        top_5_percent = list(
+            map(lambda x: (x[0], 100 * x[1] / number_of_answers, 1), top_5_frequency)
+        )
 
         question_name = question_names[question_id]
         return_dict[question_name] = top_5_percent
-    
+
     return return_dict
+
 
 def _create_form(candidate_groups: list[MVPCandidates], credentials: str) -> any:
     """Creates a google form and returns the result."""
@@ -231,26 +265,25 @@ def _create_form(candidate_groups: list[MVPCandidates], credentials: str) -> any
         for candidate in candidates:
             options.append({"value": candidate})
         question_update = {
-            "requests": [{
-                "createItem": {
-                    "item": {
-                        "title": "[OPTIONAL] " + candidate_group.title,
-                        "questionItem": {
-                            "question": {
-                                "required": False,
-                                "choiceQuestion": {
-                                    "type": "RADIO",
-                                    "options": options,
-                                    "shuffle": True,
+            "requests": [
+                {
+                    "createItem": {
+                        "item": {
+                            "title": "[OPTIONAL] " + candidate_group.title,
+                            "questionItem": {
+                                "question": {
+                                    "required": False,
+                                    "choiceQuestion": {
+                                        "type": "RADIO",
+                                        "options": options,
+                                        "shuffle": True,
+                                    },
                                 }
-                            }
-                        }
-                    },
-                    "location": {
-                        "index": 0
+                            },
+                        },
+                        "location": {"index": 0},
                     }
                 }
-            }
             ]
         }
         service.forms().batchUpdate(formId=form_id, body=question_update).execute()
@@ -260,15 +293,18 @@ def _create_form(candidate_groups: list[MVPCandidates], credentials: str) -> any
 def _add_permissions_to_formid(form_id, google_credentials):
     """Adds write permissions to all mods for the form."""
     # Add all questions.
-    service = build('drive', 'v3', credentials=google_credentials)
+    service = build("drive", "v3", credentials=google_credentials)
 
     batch = service.new_batch_http_request()
     for email in rleb_settings.moderator_emails:
-        permission = {
-            'type': 'user',
-            'role': 'writer',
-            'emailAddress': email
-        }
-        batch.add(service.permissions().create(fileId=form_id,body=permission,fields='id',sendNotificationEmail=False))
+        permission = {"type": "user", "role": "writer", "emailAddress": email}
+        batch.add(
+            service.permissions().create(
+                fileId=form_id,
+                body=permission,
+                fields="id",
+                sendNotificationEmail=False,
+            )
+        )
 
     batch.execute()
--- rleb_liqui/rleb_prizepool_lookup.py
+++ rleb_liqui/rleb_prizepool_lookup.py
@@ -8,19 +8,22 @@ from rleb_liqui import rleb_liqui_utils
 from rleb_stdout import print_to_channel
 import rleb_settings
 
-async def handle_prizepool_lookup(liquipedia_url: str , channel: str) -> str:
+
+async def handle_prizepool_lookup(liquipedia_url: str, channel: str) -> str:
     """Gets prizepool markdown from a liquipedia tournament page."""
 
-    rleb_settings.rleb_log_info("Prizepool: Creating prizepool lookup for {0}".format(liquipedia_url))
+    rleb_settings.rleb_log_info(
+        "Prizepool: Creating prizepool lookup for {0}".format(liquipedia_url)
+    )
 
     page = None
     try:
-         page = rleb_liqui_utils.get_page_html_from_url(liquipedia_url)
+        page = rleb_liqui_utils.get_page_html_from_url(liquipedia_url)
     except Exception as e:
         await channel.send("Couldn't load {0}!\nError: {1}".format(liquipedia_url, e))
         rleb_settings.rleb_log_info(
-                "MVP: Couldn't load {0}!\nError: {1}".format(liquipedia_url, e)
-            )
+            "MVP: Couldn't load {0}!\nError: {1}".format(liquipedia_url, e)
+        )
         rleb_settings.rleb_log_error(traceback.format_exc())
 
     html = BeautifulSoup(page, "html.parser")
@@ -28,9 +31,9 @@ async def handle_prizepool_lookup(liquipedia_url: str , channel: str) -> str:
     # Get all rows of prizepool table, ignore the first row which contains table headers.
     team_rows = html.select("table.prizepooltable")[0].select("tr:not(:first-child)")
 
-    markdown = '|**Place**|**Prize**|**Team**|**RLCS Points**|\n|:-|:-|:-|:-|'
+    markdown = "|**Place**|**Prize**|**Team**|**RLCS Points**|\n|:-|:-|:-|:-|"
 
-    # This part is actually a bit complex because liqui uses tricky CSS to create two 3rd-4th places in tourneys where there is no difference between 3rd and 4th place. 
+    # This part is actually a bit complex because liqui uses tricky CSS to create two 3rd-4th places in tourneys where there is no difference between 3rd and 4th place.
     # In tourneys where 3rd and 4th place are equivalent, we need to keep track of the prize & point total from 3rd place because it's missing from 4th place.
     team_place = None
     prize = None
@@ -39,7 +42,7 @@ async def handle_prizepool_lookup(liquipedia_url: str , channel: str) -> str:
     for i in range(min(8, len(team_rows))):
         team_data = team_rows[i].select("td")
         # Partially down the prizepool, there's a row that just houses a toggle for the UI,
-        if team_data[0].get('class') == "prizepooltabletoggle":
+        if team_data[0].get("class") == "prizepooltabletoggle":
             continue
         team_name = None
         if len(team_data) == 4:
@@ -51,6 +54,7 @@ async def handle_prizepool_lookup(liquipedia_url: str , channel: str) -> str:
         else:
             # If this is a followup row, use prize and points from previous row.
             team_name = team_data[0].text.strip()
-        markdown += f'\n|**{team_place}** | {prize} | {team_name} | +{new_points} **()** |'
+        markdown += (
+            f"\n|**{team_place}** | {prize} | {team_name} | +{new_points} **()** |"
+        )
     await print_to_channel(channel, markdown)
-    
