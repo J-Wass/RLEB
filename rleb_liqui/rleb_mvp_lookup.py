a1df85bca4968f57b8d3583d2ff4fbcd642491b6
j-wass <jonahwasserman97@gmail.com>
2022-08-07 19:56:05 -0700

    make !mvp support new roster tables from liqui

--- rleb_liqui/rleb_mvp_lookup.py
+++ rleb_liqui/rleb_mvp_lookup.py
@@ -48,6 +48,9 @@ class MVPCandidates:
         self.title = title
         self.candidates = candidates
 
+    def __repr__(self):
+        return f'{self.title} -> {self.candidates}'
+
 
 async def handle_mvp_form_creation(liquipedia_urls: list[str], channel) -> None:
     """Handles a request to get markdown for a new post about mvps from each liqui url."""
@@ -108,7 +111,7 @@ async def _get_mvp_candidates(
 
 
 async def _get_single_mvp_candidate_group(
-    liquipedia_url: str, channel
+    liquipedia_url: str, channel, teams_allowed: int = 4
 ) -> Optional[MVPCandidates]:
     """Gets an MVPCandidate group from the liquipedia_url."""
     page = None
@@ -124,29 +127,49 @@ async def _get_single_mvp_candidate_group(
 
     html = BeautifulSoup(page, "html.parser")
 
-    teams_allowed = 4
-
-    teams_box = html.select("div[class^=teamcard-columns]")[0]
-    teams = teams_box.select("div.template-box")
-
-    # Create mapping of team_name to player name.
     mvp_candidates = {}
-    for team in teams:
-        team_name = team.select("center > a")[0].text
-        player_table = team.select("div.teamcard-inner > table")[0]
-        players = player_table.select("td")
-        candidates = []
-        for i in range(3):
-            player_name = players[i].text.strip()
-            candidates.append(f"{player_name} ({team_name})")
-        mvp_candidates[team_name] = candidates
+
+    teams_boxes = html.select("div[class^=teamcard-columns]")
+    for teams_box in teams_boxes:
+        teams = teams_box.select("div.template-box")
+
+        # Create mapping of team_name to player name.
+        for team in teams:
+            team_name = team.select("center > a")[0].text
+            player_tables = team.select("div.teamcard-inner > table")
+            for player_table in player_tables:
+                players = player_table.select("td")
+                candidates = []
+                for i, player in enumerate(players):
+                    player_name = player.text.strip()
+
+                    # Filter out bad liqui data
+                    bad_rows = ["DNP", "Ranking", "Substitutes", "Main Roster"]
+                    if any(bad_row in player_name for bad_row in bad_rows):
+                        continue
+                    if player_name == "":
+                        continue
+
+                    candidates.append(f"{player_name} ({team_name})")
+                if team_name not in mvp_candidates:
+                    mvp_candidates[team_name] = []
+                mvp_candidates[team_name].extend(candidates)
 
     # Get all players on top teams.
     eligible_candidates = []
-    prizepool = html.select("table.prizepooltable")[0]
-    rows = prizepool.select("span[class^='team-template-team']")
+    prizepool = html.select("table.prizepooltable:not(.collapsed)")
+    
+    # Liqui can render the prizepool as a div or table lol.
+    if not prizepool or len(prizepool) == 0:
+        prizepool = html.select("div.general-collapsible.prizepooltable")
+        rows = prizepool[0].select("span.name")
+    else:
+        rows = prizepool[0].select("span[class^='team-template-team']")
+
     for i in range(min(teams_allowed, len(rows))):
         team_name = rows[i].text.strip()
+        if team_name == "TBD" or  team_name == "":
+            continue
         eligible_candidates = eligible_candidates + mvp_candidates[team_name]
 
     title = " | ".join(liquipedia_url.split("/")[4:]).replace("_", " ")
@@ -259,7 +282,6 @@ def _create_form(candidate_groups: list[MVPCandidates], credentials: str) -> any
 
     # Build a question for each candidate group as a form update.
     groups = candidate_groups.copy()
-    random.shuffle(groups)
     for candidate_group in groups:
         options = []
 
