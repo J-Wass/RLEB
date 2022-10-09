41d5ce98c02462ab361f1e1603f20389b45e73c9
j-wass <jonahwasserman97@gmail.com>
2022-10-09 13:39:14 -0700

    update weekly sched text

--- rleb_calendar.py
+++ rleb_calendar.py
@@ -42,7 +42,7 @@ Events prefixed with ⚽ will have an **Event Thread** posted by mods.  Anyone c
 
 If you are on the official **Reddit App**, you will find the schedule under the "**About**" tab. If you are browsing the **Desktop** version,  you can find the schedule on the sidebar of the [**new.reddit**](https://new.reddit.com/r/RocketLeagueEsports/) version of this subreddit. Alternatively you can use our [**Google Calendar**](https://www.reddit.com/r/RocketLeagueEsports/wiki/calendar) which is used to feed the schedule.
 
-# RLCS 2021-22 Wiki Pages
+# RLCS 2022-23 Wiki Pages
 
 ▶ [**How it works**](https://www.reddit.com/r/RocketLeagueEsports/wiki/guide)
 
