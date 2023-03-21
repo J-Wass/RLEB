67f80edf08f4a3ae2038cb9e50b5bed2a29a5db5
j-wass <jonahwasserman97@gmail.com>
2023-03-20 22:40:22 -0400

    make that thread

--- liqui/diesel.py
+++ liqui/diesel.py
@@ -6,6 +6,28 @@ from liqui.liqui_utils import string_to_base64, base64_to_string
 import global_settings
 import stdout
 
+
+
+async def handle_makethread_lookup(url: str, template: str, day_number: int,channel: discord.channel.TextChannel) -> None:
+    """Handle broadcast stream table lookup message.
+
+    Args:
+        channel (discord.channel.TextChannel): Channel the lookup is being used in.
+        url (str): Liquipedia URL string to look for broadcast streams.
+    """
+    global_settings.rleb_log_info("DIESEL: Creating broadcast stream lookup for {0}".format(url))
+
+    try:
+        response = requests.get(
+            f"http://127.0.0.1:8080/makethread/{string_to_base64(url)}/template/{string_to_base64(template)}/day/{day_number}"
+        )
+        markdown = base64_to_string(response.content)
+        await stdout.print_to_channel(channel, markdown, title="Thread", force_pastebin=True)
+        return
+    except Exception as e:
+        await channel.send("Failed to build thread with !makethread :(")
+        await channel.send(e)
+
 async def handle_broadcast_lookup(url: str, channel: discord.channel.TextChannel) -> None:
     """Handle broadcast stream table lookup message.
 
