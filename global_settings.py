0768883352bff827c1ca6373926ec9c4f20fd442
iargue <wtymdjs@gmail.com>
2026-06-02 17:08:09 -0500

    Add online pings
    Adds the option to ping members that are part of Mod Que Warriors role and are listed as online in discord.

--- global_settings.py
+++ global_settings.py
@@ -201,8 +201,11 @@ MODERATION_CHANNEL_ID = int(
     or config["Discord"]["MODERATION_CHANNEL_ID"]  # type: ignore
 )
 BOT_LOGS_CHANNEL_ID = int(
-    os.environ.get("BOT_LOGS_CHANNEL_ID")
-    or config["Discord"]["BOT_LOGS_CHANNEL_ID"]  # type: ignore
+    os.environ.get("BOT_LOGS_CHANNEL_ID") or config["Discord"]["BOT_LOGS_CHANNEL_ID"]  # type: ignore
+)
+MODQUE_PING_ROLE_ID = int(
+    os.environ.get("MODQUE_PING_ROLE_ID")
+    or config.get("Discord", "MODQUE_PING_ROLE_ID", fallback=1465484624678617269)  # type: ignore
 )
 
 verified_needle = "verified"
@@ -216,7 +219,7 @@ if RUNNING_MODE == "local":
     MODLOG_CHANNEL_ID = BOT_COMMANDS_CHANNEL_ID
     MODERATION_CHANNEL_ID = BOT_COMMANDS_CHANNEL_ID
     ROSTER_NEWS_CHANNEL_ID = BOT_COMMANDS_CHANNEL_ID
-    #BOT_LOGS_CHANNEL_ID = BOT_COMMANDS_CHANNEL_ID
+    # BOT_LOGS_CHANNEL_ID = BOT_COMMANDS_CHANNEL_ID
 
 
 colors = [
